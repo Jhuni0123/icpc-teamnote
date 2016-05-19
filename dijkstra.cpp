2d8c6ed1ece6ffe63870006b07470eab65264ab3
Jhuni0123 <whdgnsdl887@naver.com>
2016-05-20 04:57:56 +0900

    fix dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -10,35 +10,35 @@ int n,m,a,b,c;
 int d[1111];
 vector<pii> g[1111];
 
-void dijk(int k){
-	for(int i=1;i<=n;i++){
-		d[i]=INF;
-	}
-	d[k]=0;
-	priority_queue<pii,vector<pii>,greater<pii> > pq;
-	pq.push(pii(0,k));
-	while(!pq.empty()){
-		pii p=pq.top();pq.pop();
-		int dis=p.first, v=p.second;
-		if(d[v]>dis)continue;
-		for(int i=0;i<g[v].size();i++){
-			pii q=g[v][i];
-			int to = q.first, cost = q.second;
-			if(d[to] > cost+dis){
-				pq.push(pii(d[to] = cost+dis,to));
-			}
-		}
-	}
+void dijk(vector<pii>* g,int *d,int s,int n,int k){
+    for(int i=s;i<n+s;i++){
+        d[i]=INF;
+    }
+    d[k]=0;
+    priority_queue<pii,vector<pii>,greater<pii> > pq;
+    pq.push(pii(0,k));
+    while(!pq.empty()){
+        pii p=pq.top();pq.pop();
+        int dis=p.first, v=p.second;
+        if(d[v]<dis)continue;
+        for(int i=0;i<g[v].size();i++){
+            pii q=g[v][i];
+            int to = q.first, cost = q.second;
+            if(d[to] > cost+dis){
+                pq.push(pii(d[to] = cost+dis,to));
+            }
+        }
+    }
 }
 int main(){
-	scanf("%d%d",&n,&m);
-	for(int i=0;i<m;i++){
-		scanf("%d%d%d",&a,&b,&c);
-		g[a].push_back(pii(b,c));
-	}
-	scanf("%d%d",&a,&b);
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<m;i++){
+        scanf("%d%d%d",&a,&b,&c);
+        g[a].push_back(pii(b,c));
+    }
+    scanf("%d%d",&a,&b);
 
-	dijk(a);
-	
-	printf("%d\n",d[b]);
+    dijk(g,d,1,n,a);
+    
+    printf("%d\n",d[b]);
 }

fd87e07c400bb512c1f3020dea861ea9a20425e8
Jhuni0123 <whdgnsdl887@gmail.com>
2016-08-19 20:51:03 +0900

    Update flow.cpp

--- flow.cpp
+++ flow.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 typedef struct edge{
     int to;
     int cost;
     int from;
 }edge;
-//vector<int> v[11111];
+
 vector<edge> v[11111];
 int used[11111];
 int flow(int s,int d,int mn){
@@ -20,6 +21,25 @@ int flow(int s,int d,int mn){
             if(x>0){
                 v[s][i].cost-=x;
                 v[e.to][e.from].cost+=x;
+                return x;
+            }
+        }
+    }
+    return 0;
+}
+
+vecetor<int> v[5010];
+int flow[5010][5010];
+int ford(int s,int d,int mn){
+    if(s==d)return mn;
+    used[s]=1;
+    for(int i=0;i<v[s].size();i++){
+        int e=v[s][i];
+        if(used[e]==0&&flow[s][e]>0){
+            int x=flow(e,d,min(mn,flow[s][e]));
+            if(x>0){
+                v[s][e]-=x;
+                v[e][s]+=x;
                 return x;
             }
         }

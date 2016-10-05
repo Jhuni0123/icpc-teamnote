8cc9e331844878318e580fe927b91b17d46e55c9
Jhuni0123 <whdgnsdl887@gmail.com>
2016-10-05 15:29:12 +0900

    strlen 사용을 줄임

--- kmp.cpp
+++ kmp.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void kmp(char *t, char *p, int *r, int *ff){ //
-    for(int i=0,j=0;i<strlen(t);i++){
+void kmp(char *t, char *p, int *r, int *ff){
+    int l = strlen(t)
+    for(int i=0,j=0;i<l;i++){
         if(t[i]!=p[j]){
             if(j==0)r[i]=0;
             else{

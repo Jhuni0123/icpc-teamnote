07ae62be55908aa78277f0df5216e5eadb002553
Jinpyo Kim <JinpyoKim@Jinpyos-MacBook-Pro.local>
2017-09-06 15:20:27 +0900

    Add Lazy propagation ( Jinpyo ver )

---
+++ src/Query/Lazy1.cpp
@@ -0,0 +1,74 @@
+int N,M,K;
+
+struct segTree{
+	struct Node{
+		ll d, lazy;
+	};
+	vector<Node> data;
+	int n;
+	void init(int x){
+		n = 1; while( n < x ) n *= 2;
+		data.resize(n*2+10);
+	}
+	void propagate(int node, int nodeL, int nodeR){
+		if( data[node].lazy == 0 ) return;
+		ll len = nodeR - nodeL + 1;
+		data[node].d += len*data[node].lazy;
+		if( len > 1 ){
+			data[node*2].lazy += data[node].lazy;
+			data[node*2+1].lazy += data[node].lazy;
+		}
+		data[node].lazy = 0;
+	}
+
+	void update(int l, int r, ll val, int node, int nodeL, int nodeR){
+		propagate(node, nodeL, nodeR);
+		if( l > nodeR || r < nodeL ) return;
+		if( l <= nodeL && nodeR <= r ){
+			data[node].lazy += val;
+			propagate(node,nodeL,nodeR);
+			return;
+		}
+		update(l,r,val,node*2,nodeL,(nodeL+nodeR)/2);
+		update(l,r,val,node*2+1,(nodeL+nodeR)/2+1,nodeR);
+		data[node].d = data[node*2].d + data[node*2+1].d;
+	}
+
+	ll query(int l, int r, int node, int nodeL, int nodeR){
+		propagate(node, nodeL, nodeR);
+		if( l > nodeR || r < nodeL ) return 0;
+		if( l <= nodeL && nodeR <= r ){
+			return data[node].d;
+		}
+		ll sum = 0;
+		sum += query(l,r,node*2,nodeL,(nodeL+nodeR)/2);
+		sum += query(l,r,node*2+1,(nodeL+nodeR)/2+1,nodeR);
+		return sum;
+	}
+
+};
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	segTree tree;
+	cin >> N >> M >> K;
+	tree.init(N);
+	repp(i,N){
+		ll x; cin >> x;
+		tree.update(i,i,x,1,1,tree.n);
+	}
+	repp(i,M+K){
+		int a; cin >> a;
+		if( a == 1 ){
+			int b,c; ll d;
+			cin >> b >> c >> d;
+			tree.update(b,c,d,1,1,tree.n);
+		}
+		else{
+			int b,c; cin >> b >> c;
+			printf("%lld\n",tree.query(b,c,1,1,tree.n));
+		}
+	}
+
+}

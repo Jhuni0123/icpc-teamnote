636631039d474d40a09ff1c62f1ea35cb1179067
Jhuni0123 <whdgnsdl887@naver.com>
2017-09-08 03:02:02 +0900

    Modify Rotating calipers

--- src/Geometry/RotatingCalipers.cpp
+++ src/Geometry/RotatingCalipers.cpp
@@ -1,1 +1,17 @@
-// TBD
+int main(){
+    vector<Point> convex;
+    int ans = 0;
+
+    int mid = 0;
+    // if you want iterate `only` antipodal pairs
+    // while(ccw(convex.back(), convex[0], convex[mid], convex[mid+1]) > 0) mid++;
+    
+    for(int i=0,j=mid; i < convex.size();){
+        // do something with pair of i, j
+
+        int nextj = (j+1) % convex.size();
+        int nexti = (i+1) % convex.size();
+        if (ccw(convex[i], convex[nexti], convex[j], convex[nextj]) > 0)j = nextj;
+        else i++;
+    }
+}

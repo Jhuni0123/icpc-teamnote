2dbf44c7e55012e70bcf7bcae910b526f3b7fe32
Jhuni0123 <whdgnsdl887@naver.com>
2017-09-08 01:08:35 +0900

    Renew Convex hull

--- src/Geometry/ConvexHull.cpp
+++ src/Geometry/ConvexHull.cpp
@@ -1,65 +1,49 @@
-int N,M,T,K,V;
-
-ll ccw(ll x1 ,ll y1, ll x2, ll y2, ll x3, ll y3){
-    ll ax = x2-x1; ll ay = y2-y1;
-    ll bx = x3-x1; ll by = y3-y1;
-    return (ax*by)-(bx*ay);
-}
-
-
 typedef struct Point{
     ll x,y,n;
 } Point;
 
-ll ccw(Point& a, Point& b, Point& c){
-    return ccw(a.x,a.y,b.x,b.y,c.x,c.y);
-}
-bool cmp(const Point& a, const Point& b ){
-    if( a.y == b.y ) return a.x > b.x;
-    return a.y < b.y;
+ll ccw(Point a, Point b, Point c){
+    b.x -= a.x, b.y -= a.y;
+    c.x -= a.x, c.y -= a.y;
+    return b.x*c.y - c.x*b.y;
 }
 
-Point p[100500];
-
-bool cmp_angle(const Point& a,const Point& b){
-    if( ccw(0,0,a.x,a.y,b.x,b.y) == 0 ){
-        return a.x*a.x+a.y*a.y < b.x*b.x+b.y*b.y;
+vector<Point> convex_hull(vector<Point> ps){
+    if (ps.size() < 3)return ps;
+    vector<Point> upper, lower;
+    sort(ps.begin(), ps.end(),[](const Point &a, const Point &b) {
+        if (a.x == b.x) return a.y < b.y; return a.x < b.x;
+    });
+    for(const auto &p : ps){ // ccw without `=` when include every point in convex hull
+        while(upper.size() >= 2 && ccw(*++upper.rbegin(), *upper.rbegin(), p) >= 0)upper.pop_back();
+        while(lower.size() >= 2 && ccw(*++lower.rbegin(), *lower.rbegin(), p) <= 0)lower.pop_back();
+        upper.emplace_back(p);
+        lower.emplace_back(p);
     }
-    return ccw(0,0,a.x,a.y,b.x,b.y) > 0;
+    lower.insert(lower.end(), ++upper.rbegin(), --upper.rend());
+    return lower;
 }
 
-int x[100500], y[100500];
-
-int main(void)
-{
-    geti(N);
-    repp(i,N) p[i].n = i;
-    repp(i,N) scanf("%lld%lld",&p[i].x,&p[i].y);
-    sort(p+1,p+N+1,cmp);
-    for(int i=2;i<=N;i++){
-        p[i].x -= p[1].x; p[i].y -= p[1].y;
+vector<Point> convex_hull2(vector<Point> ps){ // sorting angle
+    if (ps.size() < 3)return ps;
+    vector<Point> convex;
+    sort(ps.begin(), ps.end(), [](Point &a, Point &b){
+        if(a.x == b.x)return a.y < b.y; return a.x<b.x;
+    });
+    Point d = ps[0];
+    for(auto &p : ps){
+        p.x -= d.x;p.y -= d.y;
     }
-    
-    p[1].x = p[1].y = 0;
-    sort(p+1,p+N+1,cmp_angle);
-    
-    stack<Point> stk; stk.push(p[1]);
-    for(int i=2;i<=N;i++){
-        if( stk.size() <= 1 ){
-            stk.push(p[i]); continue;
-        }
-        Point p3 = p[i];
-        Point p2 = stk.top(); stk.pop();
-        Point p1 = stk.top(); stk.pop();
-        
-        if( ccw(p1,p2,p3) > 0 ){
-            stk.push(p1); stk.push(p2); stk.push(p3); continue;
-        }else if( ccw(p1,p2,p3) <= 0 ){
-            //p2 제거
-            stk.push(p1); i--; continue;
-        }
+    sort(ps.begin(), ps.end(), [](Point &a, Point &b){
+        if (ccw({0,0},a,b) == 0) return a.x*a.x + a.y*a.y < b.x*b.x + b.y*b.y;
+        return ccw({0,0},a,b) > 0;
+    });
+    for(auto &p : ps){
+        while(convex.size() >= 2 && ccw(*++convex.rbegin(), *convex.rbegin(), p) <= 0)convex.pop_back();
+        convex.emplace_back(p);
     }
-    
-    printf("%d",(int)stk.size());
-
+    for(auto &p : convex){
+        p.x += d.x;p.y += d.y;
+    }
+    return convex;
 }

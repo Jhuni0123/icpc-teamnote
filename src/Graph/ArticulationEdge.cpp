e5c1e0d1729f5806c99326b1ee766e6bdb2670e4
Jhuni0123 <whdgnsdl887@naver.com>
2017-09-08 03:37:46 +0900

    Change indent to 4-space

--- src/Graph/ArticulationEdge.cpp
+++ src/Graph/ArticulationEdge.cpp
@@ -8,43 +8,43 @@ set<pair<int,int>> articulation;
 // Returns the earlist discover time that x's child can visit
 // without using edge (p,x)
 int dfs(int x, int p){
-	vis[x] = ++cnt;
-	int child = 0;
-	int res = vis[x];
-	for(auto e : E[x]){
-		if(e==p) continue;
-		if(vis[e]==0){
-			// low : the earlist discover time that e can visit
-			// without using edge (x,e)
-			int low = dfs(e,x);
-			child++;
-			// keep in mind: in edge problem, low==vis[x] case
-			// is not considered as articulation edge
-			// also, root checking is not needed
-			if( low > vis[x] )
-				articulation.insert({min(e,x),max(e,x)});
-			res = min(res,low);
-		}
-		else{
-			res = min(res,vis[e]);
-		}
-	}
+    vis[x] = ++cnt;
+    int child = 0;
+    int res = vis[x];
+    for(auto e : E[x]){
+        if(e==p) continue;
+        if(vis[e]==0){
+            // low : the earlist discover time that e can visit
+            // without using edge (x,e)
+            int low = dfs(e,x);
+            child++;
+            // keep in mind: in edge problem, low==vis[x] case
+            // is not considered as articulation edge
+            // also, root checking is not needed
+            if( low > vis[x] )
+                articulation.insert({min(e,x),max(e,x)});
+            res = min(res,low);
+        }
+        else{
+            res = min(res,vis[e]);
+        }
+    }
 
-	// no root check needed for edge problem
+    // no root check needed for edge problem
 
-	return res;
+    return res;
 }
 
 int main()
 {
-	geti(N,M);
-	rep(i,M){
-		int a,b; geti(a,b);
-		E[a].pb(b); E[b].pb(a);
-	}
+    geti(N,M);
+    rep(i,M){
+        int a,b; geti(a,b);
+        E[a].pb(b); E[b].pb(a);
+    }
 
-	repp(i,N) if( vis[i] == 0 ) dfs(i,-1);
-	
-	printf("%d\n",(int)articulation.size());
-	for(auto e : articulation) printf("%d %d\n",e.first,e.second);
+    repp(i,N) if( vis[i] == 0 ) dfs(i,-1);
+
+    printf("%d\n",(int)articulation.size());
+    for(auto e : articulation) printf("%d %d\n",e.first,e.second);
 }
--- src/Graph/ArticulationPoint.cpp
+++ src/Graph/ArticulationPoint.cpp
@@ -8,40 +8,40 @@ set<int> articulation;
 // Returns the earlist discover time that x's child can visit
 // without using x  
 int dfs(int x, int p){
-	vis[x] = ++cnt;
-	int child = 0;
-	int res = vis[x];
-	for(auto e : E[x]){
-		if(vis[e]==0){
-			// low : the earlist discover time that e can visit
-			// without using x
-			int low = dfs(e,x);
-			child++;
-			// check if not root
-			if( p != -1 && low >= vis[x] ) articulation.insert(x);
-			res = min(res,low);
-		}
-		else{
-			res = min(res,vis[e]);
-		}
-	}
+    vis[x] = ++cnt;
+    int child = 0;
+    int res = vis[x];
+    for(auto e : E[x]){
+        if(vis[e]==0){
+            // low : the earlist discover time that e can visit
+            // without using x
+            int low = dfs(e,x);
+            child++;
+            // check if not root
+            if( p != -1 && low >= vis[x] ) articulation.insert(x);
+            res = min(res,low);
+        }
+        else{
+            res = min(res,vis[e]);
+        }
+    }
 
-	// check if root
-	if( p == -1 && child >= 2 ) articulation.insert(x);
+    // check if root
+    if( p == -1 && child >= 2 ) articulation.insert(x);
 
-	return res;
+    return res;
 }
 
 int main()
 {
-	geti(N,M);
-	rep(i,M){
-		int a,b; geti(a,b);
-		E[a].pb(b); E[b].pb(a);
-	}
+    geti(N,M);
+    rep(i,M){
+        int a,b; geti(a,b);
+        E[a].pb(b); E[b].pb(a);
+    }
 
-	repp(i,N) if( vis[i] == 0 ) dfs(i,-1);
-	
-	printf("%d\n",(int)articulation.size());
-	for(auto e : articulation) printf("%d ",e);
+    repp(i,N) if( vis[i] == 0 ) dfs(i,-1);
+
+    printf("%d\n",(int)articulation.size());
+    for(auto e : articulation) printf("%d ",e);
 }

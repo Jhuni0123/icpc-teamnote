f9d10540d81cf2675ba98f7595b706db27b40eef
Jhuni0123 <whdgnsdl887@naver.com>
2017-11-09 00:59:09 +0900

    Fix indent to space

--- src/Miscelleneous/PersistenceSegTree.cpp
+++ src/Miscelleneous/PersistenceSegTree.cpp
@@ -2,43 +2,43 @@ int n, cnt;
 int root[MAXN];
 
 struct node {
-	int sum, left, right;
+    int sum, left, right;
 } tree[3 * MAXN * LOGN];
 
 int build(int l = 0, int r = n) {
-	int idx = ++cnt;
-	if(r - l <= 1) {
-		tree[idx] = {0, 0, 0};
-		return idx;
-	}
-	int mid = (l + r) >> 1;
-	tree[idx] = {0, build(l, mid), build(mid, r)};
-	return idx;
+    int idx = ++cnt;
+    if(r - l <= 1) {
+        tree[idx] = {0, 0, 0};
+        return idx;
+    }
+    int mid = (l + r) >> 1;
+    tree[idx] = {0, build(l, mid), build(mid, r)};
+    return idx;
 }
 
 int update(int x, int prev, int l = 0, int r = n) {
-	if(x < l || r <= x) return prev;
-	int idx = ++cnt;
-	if(r - l <= 1) {
-		tree[idx] = {1, 0, 0};
-		return idx;
-	}
+    if(x < l || r <= x) return prev;
+    int idx = ++cnt;
+    if(r - l <= 1) {
+        tree[idx] = {1, 0, 0};
+        return idx;
+    }
 
-	int mid = (l + r) >> 1;
-	int L = update(x, tree[prev].left, l, mid);
-	int R = update(x, tree[prev].right, mid, r);
-	tree[idx] = {tree[L].sum + tree[R].sum, L, R};
-	return idx;
+    int mid = (l + r) >> 1;
+    int L = update(x, tree[prev].left, l, mid);
+    int R = update(x, tree[prev].right, mid, r);
+    tree[idx] = {tree[L].sum + tree[R].sum, L, R};
+    return idx;
 }
 
 int query(int x, int y, int k, int l = 0, int r = n) {
-	if(r - l <= 1) return l;
-	int mid = (l + r) >> 1;
-	int leftSum = tree[tree[y].left].sum - tree[tree[x].left].sum;
-	if(leftSum >= k)
-		return query(tree[x].left, tree[y].left, k, l, mid);
-	else
-		return query(tree[x].right, tree[y].right, k - leftSum, mid, r);
+    if(r - l <= 1) return l;
+    int mid = (l + r) >> 1;
+    int leftSum = tree[tree[y].left].sum - tree[tree[x].left].sum;
+    if(leftSum >= k)
+        return query(tree[x].left, tree[y].left, k, l, mid);
+    else
+        return query(tree[x].right, tree[y].right, k - leftSum, mid, r);
 }
 
 
@@ -47,25 +47,25 @@ int a[MAXN], rev[MAXN];
 map<int, int> M;
 
 int main() {
-	int q;
-	geti(n, q);
-	for(int i = 1; i <= n; i++) {
-		geti(a[i]);
-		rev[i-1] = a[i];
-	}
-	sort(rev, rev + n);
-	for(int i = 0; i < n; i++)
-		M[rev[i]] = i;
-	for(int i = 1; i <= n; i++)
-		a[i] = M[a[i]];
+    int q;
+    geti(n, q);
+    for(int i = 1; i <= n; i++) {
+        geti(a[i]);
+        rev[i-1] = a[i];
+    }
+    sort(rev, rev + n);
+    for(int i = 0; i < n; i++)
+        M[rev[i]] = i;
+    for(int i = 1; i <= n; i++)
+        a[i] = M[a[i]];
 
-	root[0] = build();
-	for(int i = 1; i <= n; i++)
-		root[i] = update(a[i], root[i-1]);
+    root[0] = build();
+    for(int i = 1; i <= n; i++)
+        root[i] = update(a[i], root[i-1]);
 
-	while(q--) {
-		int i, j, k;
-		geti(i, j, k);
-		printf("%d\n", rev[query(root[i-1], root[j], k)]);
-	}
+    while(q--) {
+        int i, j, k;
+        geti(i, j, k);
+        printf("%d\n", rev[query(root[i-1], root[j], k)]);
+    }
 }

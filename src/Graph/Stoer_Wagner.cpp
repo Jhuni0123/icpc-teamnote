dd00806c94bdd95ed13485b2c848362963fdd14a
Jhuni0123 <whdgnsdl887@naver.com>
2017-11-09 00:42:57 +0900

    Add stoer wagner

---
+++ src/Graph/Stoer_Wagner.cpp
@@ -0,0 +1,60 @@
+// Stoer-Wagner algorithm
+struct mincut {
+	int n;
+	vector<vector<int>> graph;
+
+	void init(int nn) {
+		n = nn;
+		graph.resize(n, vector<int>(n, 0));
+	}
+
+	void addEdge(int u, int v, int w) {
+		graph[u][v] += w;
+		graph[v][u] += w;
+	}
+
+	pair<int, vector<int>> findMincut() {
+		vector<vector<int>> weight = graph;
+		vector<bool> used(n, 0);
+		vector<int> best_cut;
+		int best_weight = -1;
+		
+		vector<vector<int>> group(n);
+		for(int i = 0; i < n; i++) 
+			group[i].push_back(i);
+		
+
+		for(int phase = n-1; phase >= 0; phase--) {
+			int start = 0;
+			vector<int> w = weight[start];
+			vector<bool> inSet = used;
+			inSet[start] = true;
+			int prev, last = start;
+
+			for(int i = 0; i < phase; i++) {
+				prev = last;
+				last = -1;
+				for(int j = 0; j < n; j++)
+					if(!inSet[j] && (last == -1 || w[j] > w[last])) last = j;
+
+				if(i < phase-1) {
+					inSet[last] = true;
+					for(int j = 0; j < n; j++)
+						w[j] += weight[last][j];
+				} else {  // last step - merge two nodes: prev & last
+					for(int j = 0; j < n; j++) {
+						weight[prev][j] += weight[last][j];
+						weight[j][prev] = weight[prev][j];
+					}
+					used[last] = true;
+					group[prev].insert(group[prev].end(), group[last].begin(), group[last].end());
+					if(best_weight == -1 || w[last] < best_weight) {
+						best_weight = w[last];
+						best_cut = group[last];
+					}
+				}
+			}
+		}
+		return make_pair(best_weight, best_cut);
+	}
+};

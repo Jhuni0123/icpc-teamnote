bb6379c90d26352ca090dc664007b4ee7cd6684d
Jhuni0123 <whdgnsdl887@gmail.com>
2022-06-27 19:41:12 +0900

    Simplify KMP

--- src/String/KMP.cpp
+++ src/String/KMP.cpp
@@ -1,31 +1,25 @@
-vector<int> preprocess(string p){
-    int m = p.size();
-    vector<int> fail(m);
-    fail[0] = 0; int j = 0;
-    for(int i=1;i<m;i++){
-        while(j>0&&p[i]!=p[j]) j = fail[j-1];
-        if( p[i] == p[j] ){
-            fail[i] = j+1; j++;
-        }else{
-            fail[i] = 0;
-        }
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> build_fail(string &p){
+    vector<int> fail(p.size());
+    int j = 0;
+    for(int i=1;i<(int)p.size();i++){
+        while (j>0 && p[i]!=p[j]) j = fail[j-1];
+        if (p[i] == p[j]) fail[i] = ++j;
     }
     return fail;
 }
 
-vector<int> kmp(string s, string p){
-    auto fail = preprocess(p);
-    vector<int> ans; int n = s.size(), m = p.size();
+vector<int> kmp(string &s, string &p){
+    auto fail = build_fail(p);
+    vector<int> ans(s.size());
     int j = 0;
-    for(int i=0;i<n;i++){
-        while(j>0 && s[i]!=p[j]) j = fail[j-1];
-        if( s[i] == p[j] ){
-            if( j == m-1 ){
-                ans.pb(i-m+1); j = fail[j];
-            }else{
-                j++;
-            }
-        }
+    for(int i=0;i<(int)s.size();i++){
+        while (j>0 && s[i]!=p[j]) j = fail[j-1];
+        if (s[i] == p[j]) ans[i] = ++j;
+        if (j == p.size()) j = fail[j-1];
     }
     return ans;
 }

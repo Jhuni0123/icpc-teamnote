353c2a2e674d163be25057b67c8baed8af707c8f
Jhuni0123 <whdgnsdl887@gmail.com>
2022-06-29 16:49:12 +0900

    Simplify manacher

--- src/String/Manacher.cpp
+++ src/String/Manacher.cpp
@@ -1,27 +1,22 @@
+#include <vector>
+#include <string>
+using namespace std;
 // finds radius of longest palindrome centered at s[i]
 // If you also want to find even-length paindromes, use dummy characters
 // baab -> #b#a#a#b#
 vector<int> ManacherAlgorithm(string s){
-    int n = (int) s.size();
+    int n = (int)s.size();
     int p = -1, r = -1;
     vector<int> A(n);
-    for(int i=0;i<n;i++){
+    for (int i=0;i<n;i++){
+        int x = r < i ? 0 : min(A[2*p - i], r-i);
+        while (i+x < n && i-x >= 0 && s[i+x] == s[i-x]) x++;
+        x--;
+        A[i] = x;
 
-        if( r < i ){
-            A[i] = 0;
-            int j = 0;
-            while( i + A[i] < n && i - A[i] >= 0 && s[ i+A[i] ] == s[ i-A[i] ] ) A[i]++;
-            A[i]--;
-        }
-        else{
-            A[i] = min( A[2*p - i] , r-i );
-            while( i + A[i] < n && i - A[i] >= 0 && s[ i+A[i] ] == s[ i-A[i] ] ) A[i]++;
-            A[i]--;
-        }
-
-        // update r
-        if( r < i + A[i] ){
-            r = i + A[i];
+        // update r, p
+        if (r < i+x){
+            r = i+x;
             p = i;
         }
     }

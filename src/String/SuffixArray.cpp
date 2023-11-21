d0b8d2e9d0b05511c7a292d8f145e955799deed0
Jonghun Park <whdgnsdl887@gmail.com>
2023-11-21 17:20:02 +0900

    Modify suffix array

--- src/String/SuffixArray.cpp
+++ src/String/SuffixArray.cpp
@@ -1,60 +1,52 @@
-// Make sure to add !, #, $, %, & at the end of input string
-class SuffixArray{
-public:
-    int n;
-    string s; 
-    vector<int> rank, temprank, sa, tempsa, c;
-    vector<int> lcp;
-    SuffixArray(string _s){
-        n = _s.size(); s = _s;
-        rank.resize(n); temprank.resize(n); sa.resize(n); tempsa.resize(n);
-        lcp.resize(n);
+#include <algorithm>
+using namespace std;
+
+// '#': 35, '~': 126
+struct SuffixArray {
+    string s;
+    vector<int> sa, lcp;
+    SuffixArray(string &s) : s(s), sa(s.size()), lcp(s.size()) {
         constructSA();
         constructLCP();
     }
 
-    void countingSort(int k){
-        int sum = 0, maxi = max(270, n); //ASCII 256 
-        c.clear(); c.resize(maxi+10);
-        for(auto& e : c ) e = 0;
-        for(int i=0; i<n; i++) c[ i+k<n ? rank[i+k] : 0 ] ++;
-        for(int i=0; i<maxi; i++){
-            int t = c[i]; c[i] = sum; sum += t;
+    vector<int> constructSA() {
+        int n = s.size();
+        vector<int> rank(n+1), tmp(n+1);
+        int c = max(n, 256);
+        vector<int> cnt(c+2), idx(n);
+        for(int i=0;i<n;i++)sa[i] = i, rank[i] = s[i];
+        for(int k=1;k<n;k<<=1) {
+            fill(cnt.begin(), cnt.end(), 0);
+            for(int i=0;i<n;i++) cnt[rank[min(i+k, n)]+1]++;
+            for(int i=1;i<=c;i++) cnt[i] += cnt[i-1];
+            for(int i=0;i<n;i++) idx[cnt[rank[min(i+k, n)]]++] = i;
+
+            fill(cnt.begin(), cnt.end(), 0);
+            for(int i=0;i<n;i++) cnt[rank[i]+1]++;
+            for(int i=1;i<=c;i++) cnt[i] += cnt[i-1];
+            for(int i=0;i<n;i++) sa[cnt[rank[idx[i]]]++] = idx[i];
+
+            int r = tmp[sa[0]] = 1;
+            for(int i=1;i<n;i++) tmp[sa[i]] = r += (rank[sa[i-1]] == rank[sa[i]] ? rank[sa[i-1]+k] < rank[sa[i]+k] : rank[sa[i-1]] < rank[sa[i]]);
+            rank.swap(tmp);
+            if (r == n)break;
         }
-        for(int i=0; i<n; i++) tempsa[ c[ sa[i]+k < n ? rank[sa[i]+k] : 0 ] ++ ] = sa[i];
-        for(int i=0; i<n; i++) sa[i] = tempsa[i];
+        return sa;
     }
 
-
-    void constructSA(){
-        for(int i=0; i<n; i++) rank[i] = s[i];
-        for(int i=0; i<n; i++) sa[i] = i;
-        for(int k=1; k<n; k<<=1){
-            countingSort(k);
-            countingSort(0);
-            int r = 0;
-            temprank[sa[0]] = 0;
-            for(int i=1; i<n; i++){
-                temprank[sa[i]] = (rank[sa[i]] == rank[sa[i-1]] && rank[sa[i]+k] == rank[sa[i-1]+k] ) ? r : ++r;
-            }
-            for(int i=0; i<n; i++) rank[i] = temprank[i];
-            if( rank[sa[n-1]] == n-1 ) break;
-        }
-    }
-
-
-    // lcp Implementation from
-    // http://m.blog.naver.com/dark__nebula/220419358547
-    void constructLCP(){
+    vector<int> constructLCP() {
+        int n = s.size();
+        vector<int> rank(n);
+        for(int i=0;i<n;i++) rank[sa[i]] = i;
         int h = 0;
-        for(int i=0;i<n;i++){
-            if( rank[i] ){
-                int j = sa[rank[i]-1];
-                while( s[i+h] == s[j+h] ) h++;
-                lcp[rank[i]] = h;
-            }
-            if( h > 0 ) h--;
+        for(int i=0;i<n;i++) {
+            if (!rank[i])continue;
+            int j = sa[rank[i]-1];
+            while(i+h<n && j+h<n && s[i+h] == s[j+h])h++;
+            lcp[rank[i]] = h;
+            if (h > 0)h--;
         }
+        return lcp;
     }
-
 };
